70440964b200bb31ff611e2248b242981c8cb1df
Kevin Gianluca Parrino <kevin.parrino@addvalue.it>
2024-03-29 18:15:17 +0100

    Fix vari

--- frontend/reaction-time/src/app/app.component.ts
+++ frontend/reaction-time/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TimeTableComponent } from './timeTableComponent/timeTableComponent.component';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TimeTableComponent],
+  imports: [RouterOutlet, TimeTableComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- lambda/inserimento/python/inserimento.py
+++ lambda/inserimento/python/inserimento.py
@@ -11,28 +11,24 @@ class ReactionTime:
 
     def to_dict(self):
         return {
-            "id": self.id,
+            "id": self.id, # TODO rimuovere
             "time": self.time,
-            "tms_insert": self.tms_insert.strftime("%Y-%m-%d %H:%M:%S"),
+            "tms_insert": self.tms_insert.strftime("%Y-%m-%d %H:%M:%S"), # TODO rimuovere
             "user_id": self.user_id
         }
 
 def lambda_handler(event, context):
     with psycopg2.connect(dsn=os.getenv("DATABASE_DSN")) as conn:
         insert_reaction_time(1, 10.5, conn)  # Esempio di dati fittizi
-    
-    return {
-        "statusCode": 200
-    }
+
 
 def insert_reaction_time(user_id, reaction_time, db_connection):
     try:
         cursor = db_connection.cursor()
-        current_time = datetime.now()
         cursor.execute("""
-            INSERT INTO reaction_times (time, tms_insert, user_id)
-            VALUES (%s, %s, %s)
-        """, (reaction_time, current_time, user_id))
+            INSERT INTO reaction_times (time, user_id, tms_insert)
+            VALUES (%s, %s, NOW())
+        """, (reaction_time, user_id))
         db_connection.commit()
         print("Dati inseriti correttamente.")
     except Exception as e:
